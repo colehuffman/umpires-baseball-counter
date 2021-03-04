3eb206b4f93f30a2b28af28d09963d93b7743b25
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-03 22:13:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,67 @@
+// Increases Strike counter by 1
 input.onButtonPressed(Button.A, function () {
-    if (Strikes == 3) {
-        // Shows that there have been 3 strikes
-        basic.showString("3 Strikes!")
-    } else {
-        // Increases strike count by 1
-        Strikes += 1
-    }
+    // Increases strike count by 1
+    Strikes += 1
+    basic.showNumber(Strikes)
 })
-// Shows both counters
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("Pitches =")
-    basic.showString("" + (Pitches))
-    basic.clearScreen()
-    basic.pause(500)
-    basic.showString("Strikes =")
-    basic.showString("" + (Strikes))
-})
-input.onButtonPressed(Button.B, function () {
-    if (Pitches == 4) {
-        // Shows that 4 balls have been thrown
-        basic.showString("4 Balls Have Been Thrown")
+    // Shows counters, unless counters are at maximum
+    if (Balls == 4 || Strikes == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     } else {
-        // Increases ball count by 1
-        Pitches += 1
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showString("" + (Balls))
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showString("" + (Strikes))
+        basic.clearScreen()
     }
 })
-// Resets both counters
+// Increases Ball counter by 1
+input.onButtonPressed(Button.B, function () {
+    // Increases ball count by 1
+    Balls += 1
+    basic.showNumber(Balls)
+})
+// Resets Counters
 input.onGesture(Gesture.Shake, function () {
-    Pitches = 0
+    Balls = 0
     Strikes = 0
     basic.showString("Reset")
 })
-// Sets counters to zero
+// Sets counters to 0 at beginning of game
 let Strikes = 0
-let Pitches = 0
-Pitches = 0
+let Balls = 0
+Balls = 0
 Strikes = 0
+// Shows an X if Strike counter hits 3
+basic.forever(function () {
+    if (Strikes == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+// Shows an X if Ball counter hits 4
+basic.forever(function () {
+    if (Balls == 4) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
