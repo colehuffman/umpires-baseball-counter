55595ecc86f04ecfc6ba09e0e9533b1dc518afe1
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-04 12:53:43 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-// Increases Strike counter by 1
+// Increases Strike counter by 1 when A is pressed
 input.onButtonPressed(Button.A, function () {
     // Increases strike count by 1
     Strikes += 1
+    basic.showString("S")
     basic.showNumber(Strikes)
 })
 input.onButtonPressed(Button.AB, function () {
@@ -17,17 +18,20 @@ input.onButtonPressed(Button.AB, function () {
     } else {
         basic.clearScreen()
         basic.pause(200)
+        basic.showString("B")
         basic.showString("" + (Balls))
         basic.clearScreen()
         basic.pause(100)
+        basic.showString("S")
         basic.showString("" + (Strikes))
         basic.clearScreen()
     }
 })
-// Increases Ball counter by 1
+// Increases Ball counter by 1 when B is pressed
 input.onButtonPressed(Button.B, function () {
     // Increases ball count by 1
     Balls += 1
+    basic.showString("B")
     basic.showNumber(Balls)
 })
 // Resets Counters
@@ -41,9 +45,14 @@ let Strikes = 0
 let Balls = 0
 Balls = 0
 Strikes = 0
+// stops ball counter from reaching a value higher than 4
+Balls = Math.constrain(Balls, 0, 4)
+Strikes = Math.constrain(Strikes, 0, 3)
 // Shows an X if Strike counter hits 3
 basic.forever(function () {
+    // If strike counter is equal to 3. an S and an X are displayed to symbolize that 3 strikes have occurred and the game is over
     if (Strikes == 3) {
+        basic.showString("S")
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -53,9 +62,10 @@ basic.forever(function () {
             `)
     }
 })
-// Shows an X if Ball counter hits 4
+// If ball counter is equal to 4. a B and an X are displayed to symbolize that 4 balls have been pitched and the game is over
 basic.forever(function () {
-    if (Balls == 4) {
+    if (Balls >= 4) {
+        basic.showString("B")
         basic.showLeds(`
             # . . . #
             . # . # .
